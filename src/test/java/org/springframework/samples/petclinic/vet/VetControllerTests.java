cf3feef168a42b5aeb324955e3e4d82678b7949f
SergioEB <segio.est.br@gmail.com>
2022-11-09 08:04:44 +0100

    Refactor

--- src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/vet/VetControllerTests.java
@@ -25,6 +25,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.samples.petclinic.specialty.Specialty;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
